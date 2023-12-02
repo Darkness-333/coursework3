ca23d142c0af32372c500a8d57b5429e9db2ab15
Darkness-333 <darkness-33@mail.ru>
2023-12-02 20:33:51 +0300

    defer

--- General.js
+++ General.js
@@ -1,32 +1,33 @@
-window.onload=function(){
-  let checkbox=document.getElementById("menu-toggle");
-  let n=document.getElementsByTagName("nav");
+
+let checkbox=document.getElementById("menu-toggle");
+let n=document.getElementsByTagName("nav");
 
 
-  if(window.innerWidth<1200){
-    console.log(checkbox);
-    let isCheck=localStorage.getItem("check");
-    if(isCheck=="true") checkbox.checked=true;
-    else checkbox.checked=false;
-    if(!checkbox.checked) n[0].style.display="none";
-    else n[0].style.display="block";
-  }
 
-  checkbox.onclick=function(){
-  localStorage.setItem("check",checkbox.checked);
-    if(!checkbox.checked) n[0].style.display="none";
-    else n[0].style.display="block";
-  }
-
-  window.onresize = function() {
-    if (window.innerWidth>1200){
-      n[0].style.display="flex";
-    }
-    else{
-      if(!checkbox.checked) n[0].style.display="none";
-      else n[0].style.display="block";
-    }
-  };
+if(window.innerWidth<1200){
+  console.log(checkbox);
+  let isCheck=localStorage.getItem("check");
+  if(isCheck=="true") checkbox.checked=true;
+  else checkbox.checked=false;
+  if(!checkbox.checked) n[0].style.display="none";
+  else n[0].style.display="block";
 }
+
+checkbox.onclick=function(){
+localStorage.setItem("check",checkbox.checked);
+  if(!checkbox.checked) n[0].style.display="none";
+  else n[0].style.display="block";
+}
+
+window.onresize = function() {
+  if (window.innerWidth>1200){
+    n[0].style.display="flex";
+  }
+  else{
+    if(!checkbox.checked) n[0].style.display="none";
+    else n[0].style.display="block";
+  }
+};
+
 
 
