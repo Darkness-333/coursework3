8e9b4dbe58b22466bafb23106d1898b042bf4377
Darkness-333 <darkness-33@mail.ru>
2023-12-02 20:00:08 +0300

    checkbox problem4

--- General.js
+++ General.js
@@ -1,35 +1,32 @@
-let checkbox;
-let n;
-let cont;
-
 window.onload=function(){
-  checkbox=document.getElementById("menu-toggle");
-  n=document.getElementsByTagName("nav");
-  cont=document.getElementById("burger-container");
-}
+  let checkbox=document.getElementById("menu-toggle");
+  let n=document.getElementsByTagName("nav");
 
 
-if(window.innerWidth<1200){
+  if(window.innerWidth<1200){
     console.log(checkbox);
     let isCheck=localStorage.getItem("check");
     if(isCheck=="true") checkbox.checked=true;
     else checkbox.checked=false;
     if(!checkbox.checked) n[0].style.display="none";
     else n[0].style.display="block";
-}
-
-checkbox.onclick=function(){
-localStorage.setItem("check",checkbox.checked);
-  if(!checkbox.checked) n[0].style.display="none";
-  else n[0].style.display="block";
-}
-
-window.onresize = function() {
-  if (window.innerWidth>1200){
-    n[0].style.display="flex";
   }
-  else{
+
+  checkbox.onclick=function(){
+  localStorage.setItem("check",checkbox.checked);
     if(!checkbox.checked) n[0].style.display="none";
     else n[0].style.display="block";
   }
-};
+
+  window.onresize = function() {
+    if (window.innerWidth>1200){
+      n[0].style.display="flex";
+    }
+    else{
+      if(!checkbox.checked) n[0].style.display="none";
+      else n[0].style.display="block";
+    }
+  };
+}
+
+
